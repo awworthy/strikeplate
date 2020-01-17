e03325ba1d5c6d0d3919905f1faedfd9a7328b47
Alex Worthy <alex.w.worthy@gmail.com>
2020-01-16 22:48:50 -0700

    Initial project commit for the mobile application
    Separated within the overall project structure in a folder so that as we
    build various components (database interface, etc.) they are separated.
    
    This skeleton has been set up for use with NFC but none of the NFC
    components have been written yet (still reading about it). The library
    imported is well-maintained so far, so it seemed like a good choice.

---
+++ nfc_mobile/android/app/src/main/kotlin/ca/macewan/c496/nfc_mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package ca.macewan.c496.nfc_mobile
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
