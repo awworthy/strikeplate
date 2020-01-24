4eac4151a11c97914ea21508a52e44be33771aa9
odlands <odlands@mymacewan.ca>
2020-01-23 18:50:14 -0700

    set up files for authorization

---
+++ nfc_mobile/lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:nfc_mobile/screens/home_page.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
