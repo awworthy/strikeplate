ed29408f54464b234df39854a065d38ef80818cc
Alex Worthy <alex.w.worthy@gmail.com>
2020-02-03 10:02:15 -0700

    Started nfc_exchange protocol

---
+++ nfc_mobile/lib/services/nfc_exchange.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:nfc_in_flutter/nfc_in_flutter.dart';
+
+
+class NFCExchange extends StatefulWidget {
+  @override
+  _NFCExchangeState createState() => _NFCExchangeState();
+}
+
+class _NFCExchangeState extends State<NFCExchange> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
