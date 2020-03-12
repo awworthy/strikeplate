16c4d195d5bbbde22aba5dd70c357df790b317a4
Alex Worthy <alex.w.worthy@gmail.com>
2020-03-12 12:06:07 -0600

    Add visual components to demo Reader application
    Builds a version of the lock image that changes based on the state of
    the application -- after a timer the image changes back to yellow (I
    hope).
    
    Proper use and loading screen not fully implemented in Homepage, but it
    has been added with the correct initialization.

--- nfc_mobile/lib/demo_reader/message_handler.dart
+++ nfc_mobile/lib/demo_reader/message_handler.dart
@@ -1,8 +1,10 @@
-import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:nfc_mobile/admin_app/shared_admin/app_bar.dart';
+import 'package:nfc_mobile/mobile_app/services/nfc_exchange.dart';
+import 'package:nfc_mobile/mobile_app/shared_mobile/drawer.dart';
+import 'package:nfc_mobile/shared/constants.dart';
 
 /// Listens for incoming signals from the server. Stateful.
 class MessageHandler extends StatefulWidget {
@@ -15,6 +17,10 @@ class _MessageHandlerState extends State<MessageHandler> {
   final Firestore _db = Firestore.instance;
   // FCM = Firebase Cloud Messaging
   final FirebaseMessaging _fcm = FirebaseMessaging();
+  NFCReader _nfcReader;
+  String _validation;
+  String _userID;
+  int _selector;
 
   /// Initialize the state of the message handler
   ///
@@ -23,14 +29,37 @@ class _MessageHandlerState extends State<MessageHandler> {
   /// the user's ID for proximity detection.
   @override
   void initState() {
+    _nfcReader = NFCReader(context);
+    _selector = 1;
     _fcm.configure(
       onMessage: (Map<String, dynamic> message) async {
-        String validation = message['notification']['validation'];
-        String userID = message['notification']['userID'];
+        _validation = message['notification']['validation'];
+        _userID = message['notification']['userID'];
 
         // Check if user was validated for entry first
-        if (validation == 'OK') {
+        if (_validation == 'OK') {
           // check if user is nearby using NFC!
+          String cast = _nfcReader.listen();
+          if (_userID == cast) {
+            // user device has broadcast the correct user ID to reader
+            setState(() {
+              _selector = 2;
+            });
+            Future.delayed(Duration(seconds: 3)).then((_) {
+              setState(() {
+                _selector = 1;
+              });
+            });
+          } // else something funky is going on here...
+        } else {
+          setState(() {
+            _selector = 3;
+          });
+          Future.delayed(Duration(seconds: 3)).then((_) {
+            setState(() {
+              _selector = 1;
+            });
+          });
         }
       }
     );
@@ -38,17 +67,15 @@ class _MessageHandlerState extends State<MessageHandler> {
 
   /// Determines the device's token and registers it
   ///
-  /// TODO: Change String ID so that it is working like the rest of the ID's
-  /// TODO: on the Firestore database.
+  /// TODO: Change String ID so that it is working like the rest of the ID's on the Firestore database.
   _saveDeviceToken() async {
-    String ID = '1234';
     String fcmToken = await _fcm.getToken();
 
     // save device token to Firestore
     if (fcmToken != null) {
       var tokens = _db
           .collection('readers')
-          .document(ID)
+          .document()
           .collection('tokens')
           .document(fcmToken);
 
@@ -59,6 +86,11 @@ class _MessageHandlerState extends State<MessageHandler> {
     }
   }
 
+  _openDoor() {
+    // Signal that the door should be opened
+
+  }
+
   /// Displays a demo page for the reader.
   ///
   /// The purpose of this is to display that a user has been successfully or
@@ -66,6 +98,46 @@ class _MessageHandlerState extends State<MessageHandler> {
   /// ie. Lock turns green or red.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: CustomAppBar(title: 'Strikeplate',),
+        drawer: MakeDrawer(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: backgroundGradient,
+          ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                      children: <Widget>[
+                        Container(
+                          height: 200,
+                        ),
+                        Container(
+                            height: 50
+                        ),
+                        ConstrainedBox(
+                            constraints: BoxConstraints(maxWidth: 240.0),
+                            child: getImage(_selector)
+                        ),
+                      ]
+                  ),
+                ]
+            ),
+          ),
+        )
+    );
+  }
+}
+
+/// Return a different image depending on whether the door should be opened.
+Image getImage(int selector) {
+  switch (selector) {
+    case 1: { return Image.asset('assets/lock_button_orange.png'); }
+    case 2: { return Image.asset('assets/lock_button_green.png'); }
+    case 3: { return Image.asset('assets/lock_button_red.png'); }
+    default: return null;
   }
 }
