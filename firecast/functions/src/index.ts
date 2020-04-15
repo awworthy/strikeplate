af71dd761e8fb5645ebc4bc297910aa645092b99
odlands <odlands@mymacewan.ca>
2020-04-15 11:53:22 -0600

    Fix cloud function
    Function was sending OK when key verification was false. This has been
    fixed.

--- firecast/functions/src/index.ts
+++ firecast/functions/src/index.ts
@@ -30,7 +30,6 @@ export const postUserID = functions.https.onRequest(async (req, res) => {
         nonceID: nonceIDVal
       })
     } else if (req.body.FunctionType ==="2") {
-      console.log(req.body.readerID);
       const docSnap = await db.collection("users").doc(req.body.userID).get()  
       .then(doc => {
         if (!doc.exists) {
@@ -73,28 +72,27 @@ export const postUserID = functions.https.onRequest(async (req, res) => {
         return false;
       });
       if(!set) {
-        const payload = { notification : { title: 'NO', body : req.body.userID.toString()}}
-        const response = await admin.messaging().sendToDevice(req.body.readerID, payload);
-        response.results.forEach((result, index) => {
-          const error = result.error;
-          if (error) {
-            console.log('Failure sending notification to reader', error);
-          } else {
-            console.log('NO message sent to reader')
-          }
-        })
         return res.status(500).json({
           message: "Error writing challenge document 2"
         })
       } else {
-        const payload = { notification : { title : 'OK', body : req.body.userID.toString()}}
+        let vMessage = "";
+        let consoleLog = "";
+        if(verified == "true") {
+          vMessage = "OK";
+          consoleLog = "OK Message sent to reader. Verification = " + verified + ". ReaderID: " + req.body.readerID.toString();
+        } else {
+          vMessage = "NO";
+          consoleLog = "NO Message sent to reader. Verification = " + verified + ". ReaderID: " + req.body.readerID.toString();
+        }
+        const payload = { notification : { title : vMessage, body : req.body.userID.toString()}}
         const response = await admin.messaging().sendToDevice(req.body.readerID, payload);
         response.results.forEach((result, index) => {
           const error = result.error;
           if (error) {
             console.log('Failure sending notification to reader', error);
           } else {
-            console.log('OK Message sent to reader')
+            console.log(consoleLog);
           }
         })
         return res.status(200).json({
@@ -132,3 +130,4 @@ async function statusReturn(nonceID: string) {
   });
   return nonceVal;
 }
+
